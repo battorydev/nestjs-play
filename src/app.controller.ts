b1fc07baa5e75913f31d07084f47c9f8600d01ad
BattoryDev <113938540+battorydev@users.noreply.github.com>
2023-10-29 20:30:24 +0700

    move file-reader service into src and make appservice call file reader service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileReaderService } from './file-reader/file-reader.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly fileReaderService: FileReaderService) {}
 
   @Get()
   getHello(): string {
+    this.fileReaderService.readFromDefaultDirectory();
     return this.appService.getHello();
   }
 }
